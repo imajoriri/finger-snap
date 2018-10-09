1a6a01c96788651a61461c44f39de5e550785b01
imajoriri <imajo.srroni@gmail.com>
2018-10-09 23:26:16 +0900

    保存機能追加

--- learning/index.py
+++ learning/index.py
@@ -15,15 +15,16 @@ import sound_data
 np.random.seed(20160615)
 tf.set_random_seed(20160615)
 
-finger_wav_files = os.listdir("./../sounds/finger")
-not_finger_wav_files = os.listdir("./../sounds/not-finger")
+project_dir = "/Users/imajo/Desktop/dev/google-assistant-mac/finger-snap/"
+finger_wav_files = os.listdir(project_dir + "./sounds/finger")
+not_finger_wav_files = os.listdir(project_dir + "./sounds/not-finger")
 
 data_len = 2048
 
 train_x = np.empty((0, data_len), int)
 train_t = np.array([])
-train_x, train_t = sound_data.add_train_data("./../sounds/finger/", finger_wav_files, train_x, train_t, 1)
-train_x, train_t = sound_data.add_train_data("./../sounds/not-finger/", not_finger_wav_files, train_x, train_t, 0)
+train_x, train_t = sound_data.add_train_data(project_dir + "./sounds/finger/", finger_wav_files, train_x, train_t, 1)
+train_x, train_t = sound_data.add_train_data(project_dir + "./sounds/not-finger/", not_finger_wav_files, train_x, train_t, 0)
 
 train_t = train_t.reshape([len(train_t), 1])
 
@@ -32,10 +33,15 @@ x, p, t, loss, train_step, correct_prediction, accuracy = learning_algorithm.dou
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+# TODO 保存を読み込む場合
+#saver.restore(sess, project_dir + "./model_data/model.ckpt")
 
 # 学習の実行
 learning.execution(2000, sess, x, p, t, loss, train_step, correct_prediction, accuracy, train_x, train_t)
 
+# TODO 保存する場合
+#saver.save(sess, project_dir + "./model_data/model.ckpt")
 # --- テスト ---
 result = sess.run(p, feed_dict={x: [train_x[0]]})
 print(train_t[0])
